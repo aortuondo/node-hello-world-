fb3a985f4ff30a81048ab2ac001344e9142db0b3
Aitor <aitor@MacBook-Pro-de-Aitor.local>
2021-10-10 01:27:49 +0200

    12

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end("Hello world!"."\n\n"."Version NodeJS: " + process.version);				
+	res.end('Hello world!'.'\n\n'.'Version NodeJS: ' + process.version);				
 });
 server.listen(3000);
 
